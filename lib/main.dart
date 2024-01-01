1a83cd37942025c65393c3d1c5381f870a931135
Musawar Hussain <hussainmusawar89@gmail.com>
2024-01-01 22:44:02 +0500

    locking device orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:expense_tracker_app/widgets/expenses.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((fn) {
+    runApp(const MyApp());
+  });
 }
 
 var kColorScheme =
